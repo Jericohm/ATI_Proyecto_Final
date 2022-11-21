c73750fd1579a595eb637b9cb362977ef5dc0c06
Jericó Hernández <72027420+Jericohm@users.noreply.github.com>
2022-11-21 03:26:41 -0600

    Update datos.js

--- src/modelos/datos.js
+++ src/modelos/datos.js
@@ -4,9 +4,27 @@ var Schema = mongoose.Schema;
 var GameSchema = Schema({
     id: Number,
     nombre: String,
-    anio: String,
-    compania: String,
-    consola: String
+    ps: Number,
+    atq: Number,
+    atq_esp: Number, 
+    def: Number,
+    def_esp: Number,
+    vel: Number,
+    tipo: String,
+    naturaleza: String,
+    evento: Number,
+    codigo: Number,
+    promocion: Number
   });
 
 module.exports = mongoose.model('informacion',GameSchema);
+
+/*
+var GameSchema = Schema({
+  id: Number,
+  nombre: String,
+  anio: String,
+  compania: String,
+  consola: String
+});
+*/
